1e362702943e0743fc50073a70e50b20bae985f6
Francesco Crisafulli <fcrisafulli@aeasley>
2019-04-05 08:09:20 -0400

    idk

--- Postgres/UserAPI/ingredient_user.py
+++ Postgres/UserAPI/ingredient_user.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+
+
 import psycopg2
 import hashlib
 
+
+class User:
+    def __init__(self):
+        self.authentication = None
+
+    def login(self):
+
+
+
 def connect(user,dbname="ingredientguru"):
     return psycopg2.connect(f"dbname={dbname} user={user}")
+
 
 def askforcreds():
     first_name = input("Enter Thou First Name")
@@ -13,16 +25,24 @@ def askforcreds():
     password = input("Enter Thou Password, its a secret")
     password = hashlib.sha224(password.encode('utf-8')).hexdigest()
 
-    return [first_name, last_name, username, email, password]
+    return [first_name, last_name, username, password, email]
+
 
 def createuser(connection,credentials):
     c = connection.cursor()
 
-    c.execute(f"INSERT INTO users VALUES('{credentials[0]}','{credentials[1]}','{credentials[2]}','{credentials[3]}','{credentials[4]}');")
+    c.execute(f"""INSERT INTO users VALUES
+            ('{credentials[0]}',
+            '{credentials[1]}',
+            '{credentials[2]}',
+            '{credentials[3]}',
+            '{credentials[4]}');""")
+
     connection.commit()
-    return "yote"
+    return "yote" # make applicable to doctest?
+
+
 if __name__ == "__main__":
     c = connect("fcrisafulli")
     creds = askforcreds()
-    createuser(c,creds)
-    
+    createuser(c,creds) 
